6e9783ea84de19ba7f35b69376c674674c8215af
Fernando Cordeiro <fernando.cordeiro@jeronimo-martins.com>
2020-03-10 10:49:30 +0000

    👌 IMPROVE: Replace controlled component for Ref

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,35 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class AddPlayerForm extends Component {
 
-	state = {
-		value: ''
-	};
+  playerInput = React.createRef();
 
-	handleValueChange = (e) => {
-		this.setState({ value: e.target.value });
-	}
+  handleSubmit = e => {
+    e.preventDefault();
+	this.props.addPlayer(this.playerInput.current.value);
+	e.currentTarget.reset()
+  };
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.addPlayer(this.state.value);
-		this.setState({ value: ''});
-	}
-
-	render() {
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <input
-					type="text"
-					value={this.state.value}
-					onChange={this.handleValueChange}
+		  type="text"
+		  ref={this.playerInput}
           placeholder="Enter a player's name"
         />
 
-        <input
-          type="submit"
-          value="Add Player"
-        />
+        <input type="submit" value="Add Player" />
       </form>
     );
   }
