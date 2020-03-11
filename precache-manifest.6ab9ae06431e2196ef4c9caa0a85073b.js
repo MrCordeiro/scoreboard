2bc85bb558ffcdd69e0e037c3be277072ee81f74
Fernando Cordeiro <fernando.cordeiro@jeronimo-martins.com>
2020-03-11 16:42:23 +0000

    Updates

---
+++ precache-manifest.6ab9ae06431e2196ef4c9caa0a85073b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adf7474566c24526528eace79a78ffd",
+    "url": "/scoreboard/index.html"
+  },
+  {
+    "revision": "f00c912f397840509680",
+    "url": "/scoreboard/static/css/main.4113cf02.chunk.css"
+  },
+  {
+    "revision": "856c85d6b5890c0a9d9b",
+    "url": "/scoreboard/static/js/2.ff6c6ff5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/scoreboard/static/js/2.ff6c6ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00c912f397840509680",
+    "url": "/scoreboard/static/js/main.6c961a36.chunk.js"
+  },
+  {
+    "revision": "b5167bb526fd07a385ff",
+    "url": "/scoreboard/static/js/runtime-main.6a5d14f7.js"
+  }
+]);
